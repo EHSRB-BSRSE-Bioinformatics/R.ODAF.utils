5bf62fb1d1b5d9dff86682b43632401e34740654
mattjmeier <matthew.james.meier@gmail.com>
2024-03-12 17:38:21 -0400

    Initial draft of functions

---
+++ R/get_DESeq_results.R
@@ -0,0 +1,228 @@
+#' Retrieve and Filter DESeq2 Analysis Results
+#'
+#' This function annotates DESeq2 results with differential expression data,
+#' performs filter-based post-processing on the DEGs, and returns a comprehensive
+#' list of outcomes including filtered and unfiltered results.
+#'
+#' @param dds DESeq2DataSet object that has been run through the DESeq pipeline.
+#' @param exp_metadata Data frame containing experimental metadata.
+#' @param contrasts Matrix where each row is a contrast with two conditions.
+#' @param design Character string specifying the primary design variable in the metadata.
+#' @param params List containing analysis parameters like adjusted p-value cutoffs.
+#' @param current_group_filter Optional current group filter string being applied.
+#' @return A list of data frames or lists containing the analysis results for DEGs, including
+#'         merged DEGs across contrasts, and various filter-based subsets of DEGs.
+#' @export
+#' @importFrom DESeq2 results lfcShrink
+#' @examples
+#' results <- get_DESeq_results(dds, metadata, contrasts_matrix, "condition", params, "treatment_group")
+get_DESeq_results <- function(dds,
+                              exp_metadata,
+                              contrasts,
+                              design,
+                              params,
+                              current_group_filter) {
+  # Initial setup for DESeq2 contrasts
+  resListAll <- list()
+  resListFiltered <- list()
+  resListDEGs <- list()
+  filtered_table <- data.frame()
+  Counts  <- counts(dds, normalized = TRUE)
+  CPMdds  <- edgeR::cpm(counts(dds, normalized = TRUE))
+  mergedDEGs <- c()
+
+  for (x in 1:nrow(contrasts)) {  # For all comparisons to be done
+    condition1 <- contrasts[x, 2] # Control
+    condition2 <- contrasts[x, 1] # Experimental
+
+    contrast_string <- paste(condition2, "vs", condition1, sep = "_")
+
+    message(contrast_string)
+
+    exp_metadata_subset <- as.matrix(exp_metadata[exp_metadata[, design] %in% c(condition1, condition2),])
+
+    # sanity checks
+    stopifnot(exprs = {
+      nrow(exp_metadata_subset) > 0
+    })
+
+    # Filter results using R-ODAF filters
+    Filter <- matrix(data = NA, ncol = 3, nrow = nrow(Counts))
+    rownames(Filter) <- rownames(Counts) # genes
+    colnames(Filter) <- c("relevance", "quantile", "spike")
+
+    # Apply the "Relevance" condition
+    message(paste0("Filtering genes: 75% of at least one group need to be above ", params$MinCount, " CPM"))
+    SampPerGroup <- table(exp_metadata_subset[, design])
+    gene_relevance_filter <- apply_relevance_filter(CPMdds, exp_metadata_subset, SampPerGroup, params$MinCount)
+    Filter[, "relevance"] <- gene_relevance_filter
+
+    compte <- Counts[gene_relevance_filter,]
+
+    Filter <- Filter[rownames(Filter) %in% rownames(compte), , drop = F]
+
+    #save all genes that are present regardless of counts
+    dfGenes <- data.frame(Ensembl_Gene_ID = rownames(Counts))
+
+    intitial_count <- nrow(dds)
+    num_relevance_filtered <- nrow(dds) - nrow(Filter)
+    message(paste0("Relevance filtering removed ", num_relevance_filtered,
+                   " genes from the ", nrow(dds)," assessed. ",
+                   nrow(Filter), " genes remaining"))
+
+    # run the DEseq p-value calculation
+    message("Obtaining the DESeq2 results")
+    currentContrast <- c(design, condition2, condition1)
+    bpparam <- MulticoreParam(params$cpus)
+    res <- DESeq2::results(dds[rownames(compte),],
+                           parallel = TRUE,
+                           BPPARAM = bpparam,
+                           contrast = currentContrast,
+                           alpha = params$alpha,
+                           pAdjustMethod = 'fdr',
+                           cooksCutoff = params$cooks) # If Cooks cutoff disabled - manually inspect.
+
+    res <- lfcShrink(dds,
+                     contrast = currentContrast,
+                     res = res,
+                     BPPARAM = bpparam,
+                     type = "ashr")
+
+    resListAll[[contrast_string]] <- res
+
+    DEsamples <- subset(res, res$padj < params$alpha)
+    if (nrow(DEsamples) == 0) {
+      print("No significant results were found for this contrast. Moving on...")
+      next
+    }
+    DECounts <- Counts[gene_relevance_filter,]
+
+    message("Check median against third quantile")
+    message("AND")
+    message("Check for the presence of a spike")
+
+    quant_spike_results <- apply_quant_spike_filters(DECounts,
+                                                     exp_metadata_subset,
+                                                     SampPerGroup,
+                                                     design)
+
+    Filter[rownames(DECounts), "quantile"] <- quant_spike_results[, "quantilePass"]
+    Filter[rownames(DECounts), "spike"] <- quant_spike_results[, "spikePass"]
+
+    # Extract the final list of DEGs
+
+    message(paste0("Filtering by linear fold-change: linear FC needs to be above ", params$linear_fc_filter_DEGs))
+
+    allCounts_all_filters <- res[rowSums(Filter) == 3 ,]
+    DECounts_real <- DEsamples[rowSums(Filter) == 3 & !is.na(DEsamples$padj) &  abs(DEsamples$log2FoldChange) > log2(params$linear_fc_filter_DEGs) ,]
+    DECounts_no_quant <- DEsamples[Filter[, 2] == 0 ,] # save these to output later
+    DECounts_spike <- DEsamples[Filter[, 3] == 0 ,] # save these to output later
+    #TODO: output quantile rule failing and spike failing genes
+
+    message(paste0("A total of ", nrow(DECounts_real),
+                   " DEGs were selected (out of ", nrow(DECounts) ,"), after ", nrow(DECounts_no_quant),
+                   " genes(s) removed by the quantile rule, ", nrow(DECounts_spike),
+                   " gene(s) with a spike, and linear fold-change filtering was applied"))
+    message("DESeq2 Done")
+
+    mergedDEGs <- c(mergedDEGs, rownames(DECounts_real))
+
+    filtered_table <- rbind(filtered_table, data.frame(facet = current_group_filter,
+                                                       contrast = contrast_string,
+                                                       initial = intitial_count,
+                                                       relevance_filtered = num_relevance_filtered,
+                                                       quantile_filtered = nrow(DECounts_no_quant),
+                                                       spike_filtered = nrow(DECounts_spike),
+                                                       passed_all_filters = nrow(DECounts_real)))
+
+    # TODO: the sapply at the end should be handling this, why doesn't it work?
+    if (nrow(DECounts_real) > 0){
+      resListDEGs[[contrast_string]] <- DECounts_real
+    }
+    if (nrow(DECounts_real) > 0){
+      resListFiltered[[contrast_string]] <- allCounts_all_filters
+    }
+
+  }
+  # If there are no significant results - remove the empty contrast from the list:
+  resListAll <- resListAll[!sapply(resListAll, is.null)]
+  resListFiltered <- resListFiltered[!sapply(resListFiltered, is.null)]
+  resListDEGs <- resListDEGs[!sapply(resListDEGs, is.null)]
+
+  mergedDEGs <- unique(mergedDEGs)
+  return(
+    list(
+      dfGenes=dfGenes,
+      resListAll=resListAll,
+      resListFiltered=resListFiltered,
+      resListDEGs=resListDEGs,
+      mergedDEGs=mergedDEGs,
+      filtered_table=filtered_table
+    )
+  )
+}
+
+
+#' Apply R-ODAF Quantile and Spike Filters to DESeq2 counts
+#'
+#' This function applies post-processing filters to differentially expressed genes (DEGs).
+#' It checks whether DEGs pass the median vs third quantile check and spike condition.
+#'
+#' @param DECounts Matrix or data frame with DEGs and their corresponding count data.
+#' @param exp_metadata_subset Data frame containing the experimental metadata subset for the current comparison.
+#' @param SampPerGroup Named vector with the count of samples per group.
+#' @param design The design variable that separates groups in the metadata subset.
+#' @importFrom data.table setDT
+#' @return A matrix with logical values indicating whether each gene passed the quantile
+#'         and spike filters, respectively ('1' indicates that the gene passed the filter).
+#' @export
+#' @examples
+#' filter_matrix <- apply_quant_spike_filters(DECounts, metadata_subset,...
+#'                                           SampPerGroup, "condition")
+
+apply_quant_spike_filters_dt <- function(DECounts, exp_metadata_subset, SampPerGroup, design) {
+# Convert to data.table
+data.table::setDT(DECounts, keep.rownames = "gene")
+data.table::setDT(exp_metadata_subset)
+
+# Calculate the quantile threshold for each group
+quantile_thresholds <- exp_metadata_subset[, .(threshold = quantile(counts, 0.75)),
+                                           by = .(group = get(design)),
+                                           .(counts = DECounts[[get(design)]])]
+
+# Merge the thresholds back into the DECounts data.table
+DECounts <- merge(DECounts, quantile_thresholds, by.x = "gene", by.y = "group")
+
+# Apply the median > quantile threshold and spike checks
+DECounts[, `:=` (quantilePass = median(counts) > threshold,
+                 spikePass = max(counts) / sum(counts) < 1.4 * (SampPerGroup[group])^(-0.66)),
+         by = .(gene)]
+
+# Return results
+Filter <- DECounts[, .(quantilePass, spikePass), by = .(gene)]
+return(Filter)
+}
+
+#' Apply Relevance Filter to Genes Based on Counts
+#'
+#' This function filters genes based on a minimum count threshold. For a gene to pass the filter,
+#' a specified proportion of its counts must be above the threshold in at least one of the conditions.
+#'
+#' @param CPMdds Matrix of normalized counts as CPM (Counts Per Million).
+#' @param exp_metadata_subset Data frame containing the experimental metadata subset for the current comparison.
+#' @param SampPerGroup Named vector with the count of samples per condition/group.
+#' @param MinCount The minimum count threshold to consider a gene as relevant.
+#' @param threshold_proportion The proportion of samples within a group that must exceed the count threshold.
+#' @return Logical vector indicating whether each gene has passed the relevance filter.
+#' @export
+apply_relevance_filter <- function(CPMdds, exp_metadata_subset, SampPerGroup, MinCount, threshold_proportion = 0.75) {
+  gene_relevance <- apply(CPMdds, 1, function(gene_counts, exp_metadata_subset, SampPerGroup) {
+    counts_pass <- sapply(names(SampPerGroup), function(group) {
+      sampleNamesGroup <- exp_metadata_subset[exp_metadata_subset[, design] == group, "original_names"]
+      sum(gene_counts[sampleNamesGroup] >= MinCount) >= threshold_proportion * SampPerGroup[group]
+    })
+    any(counts_pass) # Pass if any group has enough relevant counts
+  }, exp_metadata_subset, SampPerGroup)
+
+  return(gene_relevance)
+}
---
+++ R/process_data_and_metadata.R
@@ -0,0 +1,184 @@
+#' Process Count Data and Experimental Metadata
+#'
+#' This function applies a series of processing steps to count data and experimental metadata,
+#' which includes filtering, formatting, sorting and performing sanity checks.
+#'
+#' @param count_data A matrix or data frame of raw count data from transcriptomic analysis.
+#' @param exp_metadata A data frame containing the experimental metadata.
+#' @param contrasts A data frame of contrasts used for differential expression.
+#' @param intgroup A character vector with names of columns used as interest groups in the analysis.
+#' @param design The name of the column used for the design in the experimental metadata.
+#' @param params A list of parameters defining specific aspects of processing.
+#' @return A list containing processed count data, experimental metadata, and contrasts.
+#' @importFrom gtools mixedsort
+#' @export
+#' @examples
+#' processed_data <- process_data_and_metadata(raw_counts, metadata, contrast_matrix, c("drug","dose"), "condition", params)
+process_data_and_metadata <- function(count_data, exp_metadata, contrasts, intgroup, design, params){
+  exp_metadata <- filter_metadata(exp_metadata, params, design)
+  exp_metadata <- format_and_sort_metadata(exp_metadata, intgroup, design, params$sortcol)
+  count_data <- filter_data(count_data, exp_metadata, params$nmr_threshold)
+  if(!is.na(params$sortcol)){
+    contrasts <- sort_contrasts(exp_metadata, contrasts, design, params$sortcol)
+  }
+  check_data(count_data, exp_metadata, contrasts)
+  return(list(count_data=count_data, exp_metadata=exp_metadata, contrasts=contrasts))
+}
+
+#' Filter experimental metadata based on inclusion/exclusion criteria
+#'
+#' This function processes the experimental metadata for an
+#' experiment by excluding or including certain samples or groups based
+#' on user-defined criteria.
+#'
+#' @param exp_metadata A data frame containing the experiment metadata.
+#' @param params The list of parameters describing the study.
+#' @param design The name of the column used for the experimental design grouping.
+#' @return A filtered metadata data frame.
+#' @export
+#' @examples
+#' filter_metadata(exp_metadata, params, "group")
+filter_metadata <- function(exp_metadata, params, design) {
+  # exclude samples
+  if (any(!is.na(params$exclude_samples))) {
+    exp_metadata <- exp_metadata %>%
+      dplyr::filter(!original_names %in% params$exclude_samples)
+  }
+  # exclude groups
+  if (any(!is.na(params$exclude_groups))) {
+    exp_metadata <- exp_metadata %>%
+      dplyr::filter(!(!!sym(design)) %in% params$exclude_groups)
+    contrasts_to_filter <- exp_metadata %>%
+      dplyr::filter(!(!!sym(design)) %in% params$exclude_groups) %>%
+      pull(design) %>%
+      unique()
+    contrasts <- contrasts %>%
+      dplyr::filter(V1 %in% contrasts_to_filter)
+    if (params$strict_contrasts == T) {
+      contrasts <- contrasts %>%
+        dplyr::filter(V2 %in% contrasts_to_filter)
+    }
+  }
+  if (!is.na(params$include_only_column) & !is.na(params$include_only_group)) {
+    exp_metadata <- exp_metadata %>%
+      dplyr::filter((!!sym(params$include_only_column)) %in% params$include_only_group)
+    limit_contrasts <- exp_metadata %>%
+      pull(!!sym(design)) %>%
+      unique() %>%
+      as.character()
+    contrasts <- contrasts %>% dplyr::filter(V1 %in% limit_contrasts)
+  }
+  return(exp_metadata)
+}
+
+
+#' Format and sort experiment metadata for downstream analysis
+#'
+#' This function formats and sorts the experimental metadata by converting
+#' grouping variables (`intgroup`) to factors and sorting by another
+#' column (`sortcol`) if specified.
+#'
+#' @param exp_metadata A data frame of the experiment metadata.
+#' @param intgroup A character vector of column names to be converted to factors for grouping.
+#' @param design The name of the column used for the experimental design grouping.
+#' @param sortcol An optional column used for sorting the `design` grouping.
+#' @return The formatted and sorted metadata data frame.
+#' @export
+#' @examples
+#' format_and_sort_metadata(exp_metadata, c("treatment", "timepoint"), "treatment", "timepoint")
+format_and_sort_metadata <- function(exp_metadata, intgroup, design, sortcol) {
+  # Intgroups need to be factors for DESeq2
+  # make sure the levels are sorted for plotting later
+  for(int in intgroup){
+    exp_metadata[int] <- factor(exp_metadata[[int]], levels=gtools::mixedsort(unique(exp_metadata[[int]])))
+  }
+  # if sortcol is defined, sort the design variable based on that
+  if (!is.na(sortcol)){
+    design_factor_reordered <- factor(exp_metadata[[design]],
+                                      levels = unique(exp_metadata[[design]][mixedorder(exp_metadata[[sortcol]])]),
+                                      ordered = FALSE)
+    exp_metadata[[design]] <- design_factor_reordered
+
+    # also sort the other interesting groups we want to plot
+    for (ig in params$intgroup_to_plot){
+      intgroup_reordered <- factor(exp_metadata[[ig]],
+                                   levels = unique(exp_metadata[[ig]][mixedorder(exp_metadata[[sortcol]])]),
+                                   ordered = FALSE)
+      exp_metadata[[ig]] <- intgroup_reordered
+    }
+  }
+  return(exp_metadata)
+}
+
+#' Filter Count Data Based on Minimum Counts
+#'
+#' Removes samples from count data that have total read counts below a specified threshold.
+#' NA values within the count data are replaced with zeros.
+#'
+#' @param count_data A data frame containing count data.
+#' @param exp_metadata A data frame containing the corresponding experimental metadata.
+#' @param threshold A numeric threshold for minimum allowed count sum across samples.
+#' @return A filtered count data matrix or data frame.
+#' @export
+#' @examples
+#' filtered_count_data <- filter_data(count_data, exp_metadata, 10)
+filter_data <- function(count_data, exp_metadata, threshold){
+  # First data clean-up: replace NA & remove samples with total readcount < threshold
+  count_data[ is.na(count_data) ] <- 0
+  count_data <- count_data[,(colSums(count_data) > threshold)] # reads required per sample
+  #count_data <- count_data[(rowSums(count_data) > 1),] # reads required per gene
+  exp_metadata <- exp_metadata[exp_metadata$original_names %in% colnames(count_data),]
+  count_data <- count_data[,exp_metadata$original_names]
+  return(count_data)
+}
+
+#' Sort contrasts based on experimental metadata and a sorting column
+#'
+#' Sorts the contrasts based on the order of entries in a specified sorting column
+#' within the experimental metadata.
+#'
+#' @param exp_metadata A data frame containing the experiment metadata.
+#' @param contrasts A data frame containing contrast pairs to sort.
+#' @param design The name of the column in `exp_metadata` that corresponds to the design variable.
+#' @param sortcol The name of the column to sort by.
+#' @return A data frame of sorted contrast pairs.
+#' @export
+#' @examples
+#' sort_contrasts(exp_metadata, contrasts, "group", "timepoint")
+sort_contrasts <- function(exp_metadata, contrasts, design, sortcol) {
+  ordered_design <- exp_metadata[mixedorder(exp_metadata[,params$sortcol]),] %>%
+    dplyr::select(design) %>%
+    dplyr::pull()
+  ordered_contrasts <- contrasts %>%
+    dplyr::slice(match(ordered_design, V1)) %>%
+    unique()
+  return(ordered_contrasts)
+}
+
+#' Perform Sanity Checks on Data
+#'
+#' Runs a series of sanity checks on count data and corresponding metadata
+#' to ensure that they are consistent with each other for further analysis.
+#'
+#' @param sd A data frame of count data to check.
+#' @param exp_metadata A data frame of the experimental metadata to check against count data.
+#' @param contrasts A data frame of contrasts to validate.
+#' @return None; this function is called for its side effects.
+#' @export
+#' @examples
+#' check_data(count_data, metadata, contrasts)
+check_data <- function(sd, exp_metadata, contrasts){
+  message("Sanity checks for data")
+  # make sure they're not empty
+  stopifnot(exprs = {
+    ncol(sd) > 0
+    nrow(exp_metadata) > 0
+    nrow(contrasts) > 0
+  })
+  # Sanity check: each sample (row) in the metadata should have a corresponding column in the count data
+  stopifnot(all(exp_metadata$original_names %in% colnames(sd)))
+  # Sanity check: each column in the count data should have a corresponding sample (row) in the metadata
+  stopifnot(all(colnames(sd) %in% exp_metadata$original_names))
+  message("All OK 👍")
+}
+
